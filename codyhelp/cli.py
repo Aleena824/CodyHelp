5b52941835783c7d0cf294d386e9af784345d20e
aac4001 <aac4001@hw.ac.uk>
2026-03-12 16:23:18 +0400

    feature: adds review command with bug severity levels

--- codyhelp/cli.py
+++ codyhelp/cli.py
@@ -2,20 +2,23 @@ import click
 from openai import OpenAI
 import os
 
-client = OpenAI(
-    base_url="https://models.inference.ai.azure.com",
-    api_key=os.environ["GITHUB_TOKEN"],
-)
+api_key=os.environ.get("GITHUB_TOKEN")
+if not api_key:
+    raise EnvironmentError("GITHUB_TOKEN environment variable is not set.")
+
+client = OpenAI(base_url="https://models.inference.ai.azure.com", api_key=api_key)
 
 @click.group()
 def main():
     pass
 
+#Explain a code file.
+
 @main.command()
 @click.argument("file")
 @click.option("--interview", is_flag=True, help="Explain in interview mode")
+
 def explain(file, interview):
-    """Explain a code file."""
     try:
         with open(file, "r") as f:
             code = f.read()
@@ -28,11 +31,41 @@ def explain(file, interview):
     else:
         prompt = f"Explain what this code does in simple terms:\n\n{code}"
 
-    click.echo("Analysing...")
+    click.echo("\n ANALYSING...\n")
 
-    response = client.chat.completions.create(
+    try:
+        response = client.chat.completions.create(
         model="gpt-4o-mini",
-        messages=[{"role": "user", "content": prompt}],
-    )
+        messages=[{"role": "user", "content": prompt}])
 
-    click.echo(response.choices[0].message.content)
+        click.echo(response.choices[0].message.content)
+
+    except Exception as e:
+        click.echo(f"Error while calling API: {str(e)}")
+
+#Reviews code and suggests improvements
+
+@main.command()
+@click.argument("file")
+
+def review(file):
+    try:
+        with open (file,"r") as f:
+            code=f.read()
+    except FileNotFoundError:
+        click.echo(f"Error: File '{file}' not found.")
+        return
+    
+    prompt = f"Review this code and detect possible bugs. For each bug found, label it with a severity level: [HIGH], [MEDIUM], or [LOW]. Then suggest improvements.\n\n{code}"
+
+    click.echo("\n ANALYSING...\n")
+
+    try:
+        response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[{"role": "user", "content": prompt}])
+
+        click.echo(response.choices[0].message.content)
+
+    except Exception as e:
+        click.echo(f"Error while calling API: {str(e)}")
