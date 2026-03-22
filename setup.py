eeb3aa2a6a99b8ea4ba8e47990744e2be13a2338
aac4001 <aac4001@hw.ac.uk>
2026-03-22 20:55:25 +0400

    CodyHelp v2.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="codyhelp",
-    version="2.0.0",
+    version="2.0.1",
     description="An AI-powered CLI tool that helps developers understand and improve their code",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
